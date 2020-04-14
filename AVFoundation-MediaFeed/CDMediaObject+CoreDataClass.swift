8221bacf12091a80536ca60a508e207e53118f46
Tiffany Obi <tiffanyobi123@gmail.com>
2020-04-14 12:28:36 -0400

    created entitiies and attributes and generated the managed classes

---
+++ AVFoundation-MediaFeed/CDMediaObject+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  CDMediaObject+CoreDataClass.swift
+//  AVFoundation-MediaFeed
+//
+//  Created by Tiffany Obi on 4/14/20.
+//  Copyright © 2020 Tiffany Obi. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CDMediaObject)
+public class CDMediaObject: NSManagedObject {
+
+}
